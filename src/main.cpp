9d88876733cbec826eb694a98d051e95215cebc1
fiveseven-lambda <fiveseven.lambda@gmail.com>
2021-12-17 19:47:43 +0900

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
+#include <iostream>
+#include <memory>
+
 #include "pos.hpp"
 #include "error.hpp"
 #include "token.hpp"
+#include "lexer.hpp"
 
 int main(){
+    Lexer lexer;
+    try{
+        while(true){
+            auto token = lexer.next();
+            if(!token) break;
+            std::cout << token->pos << " ";
+            token->debug_print();
+        }
+    }catch(std::unique_ptr<error::Error> &error){
+        error->eprint(lexer.get_log());
+    }
 }
